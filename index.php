5fe6b5a25a41deb84428de9cd6a85dc89dd60528
apexusman <iam.usmansarwar@gmail.com>
2018-01-06 20:22:45 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aitheon Functions</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css">
+	<link rel="stylesheet" href="css/animate.css" type="text/css">
+	<link rel="stylesheet" href="css/mainupdated.css" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+	<div id="head">
+		<div class="container-fluid">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>    
+					<a class="navbar-brand" href="javascript:void(0);"><img src="imgs/logo.png"></a>
+				</div>
+				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-left">
+						<li><a href="javascript:void(0)">Explore</a></li>
+						<li><a href="javascript:void(0)">Support</a></li>
+						<li><a href="javascript:void(0)">About us</a></li>
+						<li><a href="javascript:void(0)">Team</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="javascript:void(0)">Register</a></li>
+						<li><a href="javascript:void(0)">Login</a></li>
+						<li><a href="javascript:void(0)">Shop</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+
+		<div class="slider-content">
+			<div class="row">
+				<div class="col-md-6 video col-xs-12">
+					<iframe width="100%" height="548" src="https://www.youtube.com/embed/yAoLSRbwxL8?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+				</div>
+				<div class="col-md-6 col-xs-12 s-c textright">
+				<div class="slide-content"> 
+					<h2>The digital ecosystem simplifying your world through A.I. and robots </h2>
+					<span>Around the world thousands of UR robots work collaboratively right alongside humans – with no safety guards and no problems.</span>
+					<div class="counter">
+						<ul>
+							<li>17<span>days</span></li>
+							<li>09<span>hours</span></li>
+							<li>32<span>mins</span></li>
+							<li>15<span>sec</span></li>
+						</ul>
+						<div class="btnlink">
+							<a href="javascript:void(0);" class="btndef">Join Wishlist</a>
+							<a href="javascript:void(0);" class="btndef">Join Wishlist</a>
+						</div>
+					</div>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container h-s-1">
+		<div class="row">
+			<div class="puresection">
+			 <div class="col-sm-12">
+				<h3>Pursue your passion, let our AI and robotics do the rest.</h3>
+			</div>
+			<div  class="col-md-3 col-sm-6 col-xs-12">
+				<ul>
+					<h4>Restaurant</h4>
+					<li>Lorem ipsum dolor</li>
+					<li>Morbi egestas nisi </li>
+					<li>Morbi ac diam vulputate</li>
+					<li>Aliquam ac nulla </li>
+				</ul>
+			</div>
+			<div  class="col-md-3 col-sm-6 col-xs-12">
+				<ul>
+					<h4>Retail</h4>
+					<li>Lorem ipsum dolor</li>
+					<li>Morbi egestas nisi </li>
+					<li>Morbi ac diam vulputate</li>
+					<li>Aliquam ac nulla </li>
+				</ul>
+			</div>
+			<div  class="col-md-3 col-sm-6 col-xs-12">
+				<ul>
+					<h4>Ecommerce</h4>
+					<li>Lorem ipsum dolor</li>
+					<li>Morbi egestas nisi </li>
+					<li>Morbi ac diam vulputate</li>
+					<li>Aliquam ac nulla </li>
+				</ul>
+			</div>
+			<div  class="col-md-3 col-sm-6 col-xs-12">
+				<ul>
+					<h4>Machine shop</h4>
+					<li>Lorem ipsum dolor</li>
+					<li>Morbi egestas nisi </li>
+					<li>Morbi ac diam vulputate</li>
+					<li>Aliquam ac nulla </li>
+				</ul>
+			</div>
+		  </div>
+		</div>
+	</div>
+
+	<div class="container h-s-2"> 
+		<div class="row"> 
+			<div class="col-md-6 col-xs-12 leftimg">
+			<div class="home-grid"> 
+				<a href="javascript:void:(0);">
+					<img class="img-responsive" src="imgs/Mechbots.jpg" alt="Mechbots">
+					<div class="fornt-caption p-right">
+						<h4>Mechbots</h4>
+						<p>Lorem ipsum dolor</p>
+						
+					</div>
+				</a>
+				<div class="hover-caption">
+				
+				<h4>Mechbots</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+tempor incididunt ut labore et dolore magna aliqua.	</p>
+			<div class="hover-icons row">
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon1.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon2.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon3.png" alt="" class="img-responsive"> </a>
+			</div>
+		
+			</div>
+				<div class="link-more">
+				<a href="javascript:" >And More...</a>
+			</div>
+				
+				</div>
+				
+			</div>	
+			<div class="home-grid"> 
+				<a href="javascript:void:(0);">
+					<img class="img-responsive" src="imgs/Aitheon-specialists.jpg" alt="Aitheon-specialists">
+					<div class="fornt-caption p-left">
+						<h4>Aitheon Specialists</h4>
+						<p>Lorem ipsum dolor</p>
+						
+					</div>
+				</a>
+				<div class="hover-caption">
+				
+				<h4>Aitheon Specialists</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+tempor incididunt ut labore et dolore magna aliqua.	</p>
+			<div class="hover-icons row">
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon1.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon2.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon3.png" alt="" class="img-responsive"> </a>
+			</div>
+		
+			</div>
+				<div class="link-more">
+				<a href="javascript:" >And More...</a>
+			</div>
+				
+				</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-xs-12">
+			<div class="home-grid"> 
+				<a href="javascript:void:(0);">
+					<img class="img-responsive" src="imgs/Digibots.jpg" alt="Digibots">
+					<div class="fornt-caption p-right">
+						<h4>Digibots</h4>
+						<p>Lorem ipsum dolor</p>
+						
+					</div>
+				</a>
+				<div class="hover-caption big">
+				
+				<h4>Digibots</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+tempor incididunt ut labore et dolore magna aliqua.	</p>
+			<div class="hover-icons row">
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon1.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon2.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon3.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon4.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon5.png" alt="" class="img-responsive"> </a>
+				</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon6.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon7.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon8.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon9.png" alt="" class="img-responsive"> </a>
+				</div>
+			</div>
+			<div class="link-more">
+				<a href="javascript:" >And More...</a>
+			</div>	
+				
+				</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-md-6 col-xs-12"> 
+			<div class="home-grid"> 
+				<a href="javascript:void:(0);">
+					<img class="img-responsive" src="imgs/Aitheon-creators.jpg" alt="Aitheon-creators">
+					<div class="fornt-caption p-left">
+						<h4>Aitheon Creators</h4>
+						<p>Lorem ipsum dolor</p>
+						
+					</div>
+				</a>
+				<div class="hover-caption">
+				
+				<h4>Aitheon Creators</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+tempor incididunt ut labore et dolore magna aliqua.	</p>
+			<div class="hover-icons row">
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon1.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon2.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon3.png" alt="" class="img-responsive"> </a>
+			</div>
+		
+			</div>
+				<div class="link-more">
+				<a href="javascript:" >And More...</a>
+			</div>
+				
+				</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-xs-12">
+			<div class="home-grid"> 
+				<a href="javascript:void:(0);">
+					<img class="img-responsive" src="imgs/Aitheon-pilots.jpg" alt="Aitheon-pilots">
+					<div class="fornt-caption p-bot-left">
+						<h4>Aitheon Pilots</h4>
+						<p>Lorem ipsum dolor</p>
+						
+					</div>
+				</a>
+				
+				<div class="hover-caption">
+				
+				<h4>Aitheon Pilots</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+tempor incididunt ut labore et dolore magna aliqua.	</p>
+			<div class="hover-icons row">
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon1.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon2.png" alt="" class="img-responsive"> </a>
+			</div>
+			<div class="col-xs-4"> 
+			<a href="javascript:"> <img src="imgs/h2-icon3.png" alt="" class="img-responsive"> </a>
+			</div>
+		
+			</div>
+				<div class="link-more">
+				<a href="javascript:" >And More...</a>
+			</div>
+				
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="h-s-3">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="graybg">
+					<h3>Join the Whitelist today </h3>
+					<a href="javascript:void:(0);" class="btndef">Join Whitelist</a>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="footermain">
+		<div class="footerinside">
+			<div  class="container">
+				<div class="row">
+					<div class="col-md-2 col-sm-6 col-xs-12">
+						<div class="ftrlink">
+							<h4>About Atheon</h4>
+							<ul>
+								<li>Who we are</li>
+								<li>Our beliefs</li>
+								<li>Our history</li>
+								<li>Corporate responsibility</li>
+								<li>Career</li>
+								<li>Exhibitions</li>
+								<li>News centre</li>
+								<li>Media</li>
+								<li>Blog</li>
+							</ul>
+						</div>
+					</div>
+					  <div class="col-md-3 col-sm-6 col-xs-12">
+							<div class="ftrlink">
+								<h4>Industries</h4>
+								<ul>
+									<li>Automotive and subcontractors</li>
+									<li>Food and tobacco production</li>
+									<li>Furniture and equipment</li>
+									<li>Corporate responsibility</li>
+									<li>Metal and machining</li>
+									<li>Plastic and polymers</li>
+									<li>Pharma and chemistry</li>
+									<li>Scientific and research</li>
+									<li>Your industry?</li>
+								</ul>
+							</div>
+					  </div>
+					  <div class="col-md-2 col-sm-6 col-xs-12">
+							<div class="ftrlink">
+								<h4>Applications</h4>
+								<ul>
+									<li>Pick and place</li>
+									<li>Injection moulding</li>
+									<li>CNC</li>
+									<li>Packaging and palletizing</li>
+									<li>Quality inspection</li>
+									<li>Assembly</li>
+									<li>Machine tending</li>
+									<li>Lab analysis and testing</li>
+									<li>Gluing and welding</li>
+									<li>Your application?</li>
+								</ul>
+							</div>
+					  </div>
+					  <div class="col-md-2 col-sm-6 col-xs-12">
+							<div class="ftrlink">
+								<h4>Products</h4>
+								<ul>
+									<li>UR3</li>
+									<li>UR5</li>
+									<li>Our history</li>
+									<li>UR10</li>
+									<li>UR robot benefits</li>
+								</ul>
+							</div>
+					  </div>
+					  <div class="col-md-3 col-sm-6 col-xs-12">
+					  	<h4>Get updates</h4>
+					  	<form class="newsletter" action="">
+					  		<input type="email" placeholder="your email address" value="">
+					  		<button type="submit" class="btndef">Join Newsletter </button>
+					  	</form>
+					  </div>
+				</div>
+				<div class="row">
+					<div class="copyrights">
+						<div class="col-md-8 col-xs-12">
+							<p>Website crafted w/ <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://fullstop360.com" target="_blank">FullStop</a>.  Copyright © 2018 Atheon. All Rights Reserved. </p>
+						</div>
+						<div class="col-md-4 col-xs-12 text-right">
+							<ul>
+								<li><a href="javascript:void(0);"><i class="fa fa-facebook" aria-hidden="true"></i> </a></li>
+								<li><a href="javascript:void(0);"><i class="fa fa-twitter" aria-hidden="true"></i> </a></li>
+								<li><a href="javascript:void(0);"><i class="fa fa-linkedin" aria-hidden="true"></i> </a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
